94427d63dd18513ac37ff5ef33ac232df9850f66
Guillermo de Pablos <guillermo.depablos@innocv.com>
2020-10-03 19:40:37 +0200

    update camara recognized

--- src/camera_stream.py
+++ src/camera_stream.py
@@ -1,8 +1,12 @@
 import cv2
 import os
+import time
 
 # Open the device at the Id 0
 camera = cv2.VideoCapture(0)
+time.sleep(1)
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 #Check whether user selected camera is opened successfully.
 if not (camera.isOpened()):
@@ -13,11 +17,23 @@ if not (camera.isOpened()):
 # camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
 
 while True:
-# Capture frame-by-frame
+    start_time = time.time()
+    # Capture frame-by-frame
     ret, frame = camera.read()
 
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY);
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5);
+
+    for(top, right, bottom, left) in faces:
+        #add frame from face
+        cv2.rectangle(frame,(top,right),(top+bottom,right+left),(0,0,255),2)
+
+    fpsInfo = "FPS: " + str(1.0/(time.time() - start_time))
+    print(fpsInfo)
+    cv2.putText(frame, fpsInfo, (10,10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255,255,255))
+
     # Display the resulting frame
-    cv2.imshow('Cámara',frame)
+    cv2.imshow('Detección de rostros',frame)
 
     #Waits for a user input to quit the application
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- src/video_stream.py
+++ src/video_stream.py
@@ -1,16 +1,24 @@
 import cv2
+import time
+
 
 # Open the device at the Id 0
-video = cv2.VideoCapture('test.mp4')
+video = cv2.VideoCapture('./data/video/test.mp4')
 
 while (video.isOpened()):
+    start_time = time.time()
     # Capture frame-by-frame
     ret, frame = video.read()
 
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # calculate FPS >> FPS = 1 / time to process loop
+    fpsInfo = "FPS: " + str(1.0/(time.time() - start_time))
+    print(fpsInfo)
+    cv2.putText(frame, fpsInfo, (10,10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255,255,255))
+
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Display the resulting frame
-    cv2.imshow('Video para pruebas',video)
+    cv2.imshow('Video para pruebas',frame)
 
     #Waits for a user input to quit the application
     if cv2.waitKey(1) & 0xFF == ord('q'):
